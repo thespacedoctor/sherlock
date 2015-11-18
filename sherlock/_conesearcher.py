c05721f671eb4f31c4b27cf7adae892a9b5e654b
thespacedoctor <nothingbutdave@gmail.com>
2015-11-18 22:02:51 +0000

    x

--- sherlock/_conesearcher.py
+++ sherlock/_conesearcher.py
@@ -173,8 +173,7 @@ class conesearcher():
         hmax = thisArray.max()
         hmin = thisArray.min()
         ratio = float(hmax - hmin + 1) / float(thisArray.size)
-        # if ratio < 100 or thisArray.size > 2000:
-        if True == True:
+        if ratio < 100 or thisArray.size > 2000:
             htmWhereClause = "where htm16ID between %(hmin)s and %(hmax)s" % locals(
             )
         else:
