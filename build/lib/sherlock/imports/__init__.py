7cbda22f7ea4c54768e00b6228063f7122255931
thespacedoctor <nothingbutdave@gmail.com>
2018-10-11 11:22:55 +0100

    removing tracking of build folder

--- build/lib/sherlock/imports/__init__.py
+++
@@ -1,7 +0,0 @@
-from milliquas import milliquas
-from ifs import ifs
-from veron import veron
-from marshall import marshall
-from _base_importer import _base_importer
-from ned import ned
-from ned_d import ned_d
