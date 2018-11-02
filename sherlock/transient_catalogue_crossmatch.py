a6c1d5b1fbaded33e065c58aeb5b64f2871cedec
thespacedoctor <nothingbutdave@gmail.com>
2018-11-02 11:14:40 +0000

    small tweaks

--- sherlock/transient_catalogue_crossmatch.py
+++ sherlock/transient_catalogue_crossmatch.py
@@ -209,6 +209,14 @@ class transient_catalogue_crossmatch():
                         allCatalogueMatches = allCatalogueMatches + catalogueMatches
                         catalogueMatches = []
 
+        associationIDs = []
+        associationIDs[:] = [xm["transient_object_id"]
+                             for xm in allCatalogueMatches]
+
+        nonAssociationTransients = []
+        nonAssociationTransients[:] = [
+            t for t in self.transients if t["id"] not in associationIDs]
+
         # ANNOTATION SEARCHES
         # ITERATE THROUGH SEARCH ALGORITHM IN ORDER
         # PRESENTED IN THE SETTINGS FILE
@@ -226,7 +234,7 @@ class transient_catalogue_crossmatch():
                         'checking physical distance crossmatches in %(search_name)s' % locals())
                     if bf in searchPara:
                         catalogueMatches = self.physical_separation_crossmatch_against_catalogue(
-                            objectList=self.transients,
+                            objectList=nonAssociationTransients,
                             searchPara=searchPara,
                             search_name=search_name + " physical",
                             brightnessFilter=bf,
@@ -239,7 +247,7 @@ class transient_catalogue_crossmatch():
                     # RENAMED from searchCatalogue
                     if bf in searchPara:
                         catalogueMatches = self.angular_crossmatch_against_catalogue(
-                            objectList=self.transients,
+                            objectList=nonAssociationTransients,
                             searchPara=searchPara,
                             search_name=search_name + " angular",
                             brightnessFilter=bf,
