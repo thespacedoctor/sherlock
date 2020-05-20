1801722bec95c06b6bd6da0098c5e2374af2918d
Dave <d.r.young@qub.ac.uk>
2020-05-20 19:23:41 +0100

    deleting stale files

--- sherlock/commonutils/__init__.py
+++ sherlock/commonutils/__init__.py
@@ -3,4 +3,5 @@
 """
 from __future__ import absolute_import
 from .update_wiki_pages import update_wiki_pages
+from .getpackagepath import getpackagepath
 from .get_crossmatch_catalogues_column_map import get_crossmatch_catalogues_column_map
